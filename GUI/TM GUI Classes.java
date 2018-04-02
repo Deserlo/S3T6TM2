90c7f08134fc8b9da7ca2ca47736ddebe2275f05
Bryant Lu <lu.bryant97@gmail.com>
2018-04-02 10:15:38 -0700

    added new java file for custom swing classes

---
+++ GUI/TM GUI Classes.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+
+class TM_Frame extends JFrame{
+	public TM_Frame(String title){
+		setTitle(title);
+		setSize(1280,720);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
