d6a8c35d461c1499d48c1e4433a79899d74eb1d4
Bryant Lu <lu.bryant97@gmail.com>
2018-04-02 13:36:44 -0700

    added a log screen panel class to GUI classes

--- GUI/TM GUI Classes.java
+++ GUI/TM GUI Classes.java
@@ -7,6 +7,43 @@ class TM_Frame extends JFrame{
 		setTitle(title);
 		setSize(1280,720);
 		setVisible(true);
+		setLayout(new BorderLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
+
+class TM_LogIn extends JPanel{
+	JTextField Username = new JTextField();
+	JPasswordField Password = new JPasswordField();
+	JLabel User = new JLabel("Username: ");
+	JLabel Pass = new JLabel("Password: ");
+	JButton Log = new JButton("Log In");
+	JButton Create = new JButton("Create Account");
+	JPanel pan = new JPanel();
+	JPanel pan1 = new JPanel();
+	JPanel pan2 = new JPanel();
+	JPanel pan3 = new JPanel();
+
+
+	public TM_LogIn(){
+		
+		Username.setColumns(10);
+		Password.setColumns(10);
+		pan.setLayout(new GridLayout(3,1,2,2));
+		pan1.add(User);
+		pan1.add(Username);
+		pan.add(pan1);
+		pan2.add(Pass);
+		pan2.add(Password);
+		pan.add(pan2);
+		pan3.add(Create);
+		pan3.add(Log);
+		pan.add(pan3);
+		add(pan);
+		
+		//setSize(384,216);
+
+	}
+}
+		
+		
