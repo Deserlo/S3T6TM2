10df8949b3acae32902b5dcc514fcf31adeab239
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-04-12 23:15:01 -0700

    Add files via upload

--- src/Manager.java
+++ src/Manager.java
@@ -1,9 +1,18 @@
+import java.util.Set;
 
 public class Manager {
+	//user session info
+	public static int mgrID = Test.userID;
+	public static boolean loggedIn = Test.login;
 	
 	public void assignProject() {
 		//assigns dev(s) a Project No. and Project Name
-		
+		//insert statements to tables works_on, project		
+	}
+	
+	public Set<String> Devs(int mgrID){
+		return null;
+		//returns list of manager's developers
 	}
 	
 	
