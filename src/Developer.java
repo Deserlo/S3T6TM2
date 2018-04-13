6658eac411dedd935a5f6bf4164441cc9e152f7c
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-04-12 23:13:55 -0700

    Add files via upload

--- src/Developer.java
+++ src/Developer.java
@@ -1,17 +1,16 @@
 
 public class Developer {
+	//user session info
 	int devID = Test.userID;
 	boolean loggedIn = Test.login;
 	
 	public void logTask(int devID) {
-			
+		//insert statements into Task table
+		//reuse code from TMModel
 	}
 	
-	public void startTask(int devID) {
-		
-	}
+	public void startTask(int devID) {}
 	
-	public void stopTask(int devID) {
-		
-	}
+	public void stopTask(int devID) {}
+	
 }
