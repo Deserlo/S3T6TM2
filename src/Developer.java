f8c55855f5842c5703918b87dee5bc75368a5840
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-04-12 17:23:14 -0700

    added signatures of initial methods

---
+++ src/Developer.java
@@ -0,0 +1,23 @@
+import java.sql.ResultSet;
+
+public class Developer {
+	
+	public ResultSet ProjectsHome(int devID) {
+		//String query = "SELECT * FROM Projects WHERE "
+		return null;
+		
+	}
+	
+	
+	public void logTask() {
+		System.out.println("logging task...");	
+		//Dev can log task only under the Projects for which dev(s) 
+		//have been assigned by their Mgr.
+	}
+	
+	public void stopTask() {
+		
+	}
+	
+
+}
