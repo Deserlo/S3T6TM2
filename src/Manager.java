409a6164d93c1e6e538a51c09a6ed0e29a8c6fca
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-04-14 02:48:45 -0700

    got more method stubs in place

--- src/Manager.java
+++ src/Manager.java
@@ -2,17 +2,33 @@ import java.util.Set;
 
 public class Manager {
 	//user session info
-	public static int mgrID = Test.userID;
-	public static boolean loggedIn = Test.login;
-	
-	public void assignProject() {
-		//assigns dev(s) a Project No. and Project Name
-		//insert statements to tables works_on, project		
+	 int mgrID = Test.userID;
+	 boolean loggedIn = Test.login;
+
+	//returns list of manager's developers by fname
+	public Set<String> Devs(int mgrID){
+		return null;	}
+	//returns list of projects manager is leading by projName
+	public Set<String> Projects(int mgrID){
+		return null;
 	}
 	
-	public Set<String> Devs(int mgrID){
+	//returns list of tasks for a selected project by taskName
+	public Set<String> TasksForProjName(int mgrID){
 		return null;
-		//returns list of manager's developers
+	}
+	//returns list of completed tasks for a selected developer
+	public Set<String> TasksForDeveloper(int devID){
+		return null;
+	}
+	
+	public void retrieveMgrProjects(int screen) {
+		switch(screen) {
+		
+		
+		
+		}
+		
 	}
 	
 	
