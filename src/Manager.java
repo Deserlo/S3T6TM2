be9148a77428fa7e2b80e1dfadd94477a6b86476
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-05-04 14:27:01 -0700

    manager display screens

--- src/Manager.java
+++ src/Manager.java
@@ -1,27 +1,251 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Set;
 
 public class Manager {
 	//user session info
 	 int mgrID = Test.userID;
 	 boolean loggedIn =Test.login; //check logged in status
+	 
+	 
+	 public String[][] getMgrHours(int mgrID){
+			String[][] noProjects = {{"No projects to display!"," "," "}, {" ", " ", " "}, {" ", " ", " "},
+													{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "},
+													{" ", " ", " "}, {" ", " ", " "},{" ", " ", " "}, {" ", " ", " "},
+													{" ", " ", " "}, {" ", " ", " "},{" ", " ", " "}, {" ", " ", " "},
+													{" ", " ", " "}, {" ", " ", " "},{" ", " ", " "}, {" ", " ", " "},
+													{" ", " ", " "}};
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			 try { 
+				   DBConnection db = new DBConnection();
+		    	   conn = db.ConnectDB();
+		    	   String query =  "SELECT P.projName, T.taskName as LastTask, P.progress as Progress " + 
+		    	   		"FROM Project P, Task T " + 
+		    	   		"WHERE  P.projNo = T.projNo and P.mgrID = ? " + 
+		    	   		"and T.duration is not null " + 
+		    	   		"GROUP BY P.ProjName " + 
+		    	   		"HAVING MAX(T.end);" ;
+		           stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		           stmt.setInt(1, mgrID);
+				   rs = stmt.executeQuery();
+				   int i=0;
+				   if (rs.last()) {
+					    int rows = rs.getRow();
+					    String [][] results =new String[rows][3];
+					    System.out.println("number of results: " + rows);
+					    // Move to beginning
+					    rs.beforeFirst();
+					    while  (rs.next()) {
+							String name = rs.getString(1);
+							String taskName = rs.getString(2);
+							float progress = rs.getFloat(3) * 100;				
+							results[i] = new String[] {name, taskName,String.valueOf(progress + "%")};
+							i++;
+						}
+						return results;
+					}
+				   	  
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					if (rs != null) try {rs.close(); } catch (SQLException ignore) {}
+					if (stmt != null) try {stmt.close(); } catch (SQLException ignore) {}
+					if (conn != null) try {conn.close(); } catch (SQLException ignore) {}
+				}
+			return noProjects; 
+		}	
+		
+	 
+	 
 
 	//returns list of manager's developers by fname
-	public String[] Devs(int mgrID){
-		return null;	
-	}
-	//returns list of projects manager is leading by projName
-	public String[] Projects(int mgrID){
-		return null;
+	public String[] getMgrDevNames(int mgrID){
+		String[] emptySet = {};
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		 try { 
+			   DBConnection db = new DBConnection();
+	    	   conn = db.ConnectDB();
+	    	   String query =  "SELECT U.Fname " + 
+	    	   		"FROM User U " + 
+	    	   		"WHERE  U.mgrID=?; ";
+	           stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+	           stmt.setInt(1, mgrID);
+			   rs = stmt.executeQuery();
+			   int i=0;
+			   if (rs.last()) {
+				    int rows = rs.getRow();
+				    String [] results =new String[rows];
+				    System.out.println("number of results: " + rows);
+				    // Move to beginning
+				    rs.beforeFirst();
+				    while  (rs.next()) {
+						String devName = rs.getString(1);
+						results[i] = devName;
+						i++;
+					}
+					return results;
+				}
+			   	  
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				if (rs != null) try {rs.close(); } catch (SQLException ignore) {}
+				if (stmt != null) try {stmt.close(); } catch (SQLException ignore) {}
+				if (conn != null) try {conn.close(); } catch (SQLException ignore) {}
+			}
+		return emptySet; 	
 	}
 	
+	//returns list of projects manager is leading by projName
+	public String[] getMgrProjects(int mgrID)
+    { 
+        String[] emptyProjects = {}; 
+        Connection conn = null; 
+        PreparedStatement stmt = null;
+        ResultSet rs = null;   
+        try
+        { 
+            DBConnection db = new DBConnection(); 
+            conn = db.ConnectDB();
+            String query = "SELECT projName " + 
+                           "FROM Manager, Project " + 
+                           "WHERE Manager.id = Project.mgrID AND mgrID = ?; ";
+            stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY );
+            stmt.setInt(1, mgrID);
+            rs = stmt.executeQuery();
+            int i = 0;
+            if(rs.last())
+            {
+                int rows = rs.getRow();
+                String[] results = new String[rows]; 
+                System.out.println("Number of results: " + rows);
+                // Move to beginning
+                rs.beforeFirst();
+                while(rs.next())
+                {
+                    String projName = rs.getString(1);
+                    results[i] = projName;
+                    i++;
+                }
+                return results;
+            }
+        }
+        catch(SQLException e)
+        { 
+            e.printStackTrace();
+        }
+        finally
+        { 
+            if(rs != null) try{rs.close();} catch (SQLException ignore) {}
+            if(stmt != null) try {stmt.close();} catch (SQLException ignore) {}
+            if(conn != null) try {conn.close();} catch (SQLException ignore){}
+        }
+        return emptyProjects;
+    }
+	
 	//returns list of tasks for a selected project by taskName
-	public String[] TasksForProjName(int mgrID){
-		return null;
+	public String[][] TasksForProjName(String projName){
+		  String[][] noTasks = {{}}; 
+	        Connection conn = null; 
+	        PreparedStatement stmt = null;
+	        ResultSet rs = null;   
+	        try
+	        { 
+	            DBConnection db = new DBConnection(); 
+	            conn = db.ConnectDB();
+	            String query = "SELECT T.taskName as Task, T.duration as Time, T.description as Description " + 
+	            		"FROM Task T, Project P " + 
+	            		"WHERE T.projNo = P.projNo and P.projName = ?; ";
+	            stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY );
+	            stmt.setString(1, projName);
+	            rs = stmt.executeQuery();
+	            int i = 0;
+	            if(rs.last())
+	            {
+	                int rows = rs.getRow();
+	                String [][] results =new String[rows][3];
+	                System.out.println("Number of results: " + rows);
+	                // Move to beginning
+	                rs.beforeFirst();
+	                while(rs.next())
+	                {
+						String name = rs.getString(1);
+						float duration = rs.getFloat(2);
+						String description = rs.getString(3);					
+						results[i] = new String[] {name,String.valueOf(duration), description};
+						i++;
+	                }
+	                return results;
+	            }
+	        }
+	        catch(SQLException e)
+	        { 
+	            e.printStackTrace();
+	        }
+	        finally
+	        { 
+	            if(rs != null) try{rs.close();} catch (SQLException ignore) {}
+	            if(stmt != null) try {stmt.close();} catch (SQLException ignore) {}
+	            if(conn != null) try {conn.close();} catch (SQLException ignore){}
+	        }
+	        return noTasks;
 	}
+	
 	//returns list of completed tasks for a selected developer
-	public String[] TasksForDeveloper(int devID){
-		return null;
-	}
+	public String[][] TasksForDeveloper(String devName)  { 
+        String[][] noTasks = {{}}; 
+        Connection conn = null; 
+        PreparedStatement stmt = null;
+        ResultSet rs = null;   
+        try
+        { 
+            DBConnection db = new DBConnection(); 
+            conn = db.ConnectDB();
+            String query = "SELECT T.taskName as Task, T.duration as Time, T.description as Description " + 
+            		"FROM Task T, User U " + 
+            		"WHERE T.devID = U.id and U.fname = ?; ";
+            stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY );
+            stmt.setString(1, devName);
+            rs = stmt.executeQuery();
+            int i = 0;
+            if(rs.last())
+            {
+                int rows = rs.getRow();
+                String [][] results =new String[rows][3];
+                System.out.println("Number of results: " + rows);
+                // Move to beginning
+                rs.beforeFirst();
+                while(rs.next())
+                {
+					String name = rs.getString(1);
+					float duration = rs.getFloat(2);
+					String description = rs.getString(3);
+									
+					results[i] = new String[] {name,String.valueOf(duration), description};
+					i++;
+
+                }
+                return results;
+            }
+        }
+        catch(SQLException e)
+        { 
+            e.printStackTrace();
+        }
+        finally
+        { 
+            if(rs != null) try{rs.close();} catch (SQLException ignore) {}
+            if(stmt != null) try {stmt.close();} catch (SQLException ignore) {}
+            if(conn != null) try {conn.close();} catch (SQLException ignore){}
+        }
+        return noTasks;
+    }
 	
 	
 }
