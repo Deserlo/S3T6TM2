d51e03fe442cf4639242ee80a1770e7693420cb2
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-05-08 10:42:59 -0700

    working on managerProjects screen

--- src/Manager.java
+++ src/Manager.java
@@ -100,7 +100,7 @@ public class Manager {
 	//returns list of projects manager is leading by projName
 	public String[] getMgrProjects(int mgrID)
     { 
-        String[] emptyProjects = {}; 
+        String[] emptyProjects = {"No Projects to display!"}; 
         Connection conn = null; 
         PreparedStatement stmt = null;
         ResultSet rs = null;   
@@ -241,6 +241,79 @@ public class Manager {
         }
         return noTasks;
     }
+	
+	
+	//{"Project","Budget","People"}
+	public String[][] getProjectsMgr(int mgrID)  { 
+        String[][] noProjects = {{"No projects to display."," "," "}};
+		PreparedStatement getPeople = null;
+        Connection conn = null; 
+        PreparedStatement stmt = null;
+        ResultSet rs = null;   
+        try
+        { 
+            DBConnection db = new DBConnection(); 
+            conn = db.ConnectDB();
+            String query = "SELECT P.projNo, P.projName, P.timeBudget " + 
+            		"FROM Project P " + 
+            		"WHERE P.mgrID = ? ";
+            stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY );
+            stmt.setInt(1, mgrID);
+            rs = stmt.executeQuery();
+            int i = 0;
+            if(rs.last())
+            {
+                int rows = rs.getRow();
+                String [][] results =new String[rows][3];
+                System.out.println("Number of results: " + rows);
+                // Move to beginning
+                rs.beforeFirst();
+                while(rs.next()){
+					int projNo = rs.getInt(1);
+					String projName = rs.getString(2);
+					float timeBudget = rs.getFloat(3);
+					getPeople = conn.prepareStatement("Select U.fname "+
+													"FROM User U, Works_on W " + 
+													"WHERE U.id = W.devID and W.projNo = ?; ");
+					getPeople.setInt(1, projNo);
+					db.rs = getPeople.executeQuery();
+					String people = "";
+					if (db.rs.last()) {
+						int x = db.rs.getRow();		
+					}
+					db.rs.beforeFirst();
+					while (db.rs.next()){
+						String person = db.rs.getString(1);
+						people += person;
+						people += person + ",";
+					}
+					results[i] = new String[] {projName, String.valueOf(timeBudget), people};
+					i++;
+                }
+                return results;
+            }
+        }
+        catch(SQLException e)
+        { 
+            e.printStackTrace();
+        }
+        finally
+        { 
+            if(rs != null) try{rs.close();} catch (SQLException ignore) {}
+            if(stmt != null) try {stmt.close();} catch (SQLException ignore) {}
+            if(conn != null) try {conn.close();} catch (SQLException ignore){}
+        }
+        return noProjects;
+    }
+	
+	public static void main(String args[] ){
+		Manager man = new Manager();
+		String [][]projs = man.getProjectsMgr(15);
+		System.out.println(projs[0][0]);
+		System.out.println(projs[0][1]);
+		System.out.println(projs[0][2]);
+	}
+	
 	
 	
 }
